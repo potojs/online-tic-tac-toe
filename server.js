2d8441fba06c63c7483e19d8de09bd6bfe2895f2
potatojs <mbenkhadra360@gmail.com>
2021-08-03 23:38:23 +0200

    removing the dotenv config

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const http = require("http").Server(app);
 const io = require("socket.io")(http);
 const Game = require("./game");
-require("dotenv").config();
 app.set("view engine", "ejs");
 
 app.use(express.static("public"));
